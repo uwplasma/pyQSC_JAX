fcda1a9218e8c3bb0d5a40f0ffd43366d278b0dc
Agastyarathee <67199509+Agastyarathee@users.noreply.github.com>
2026-06-08 06:26:52 -0500

    Change name in setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name="pyqsc_jax",
     version="0.1.0",
-    author="Agastya Rathee",
+    author="Uw_Plasma",
     packages=find_packages(),
     install_requires=[
         "jax>=0.4.0",
